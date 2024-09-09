49df016f2202b581e27080ea0d7a4b2378765db7
Yash Jankay <yashjankay@yashs-mbp.lan>
2024-09-08 21:10:06 -0700

    beam stuff in the right place?

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,12 +30,6 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
-        <Script
-          src="https://beamanalytics.b-cdn.net/beam.min.js"
-          data-token="712ffbe5-3e6e-46bf-b828-a5c7f652dac4"
-          strategy="afterInteractive"
-          async
-        />
       </body>
     </html>
   )
