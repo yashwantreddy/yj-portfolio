d3c201f19e96b278a002988006b9eab559b67369
Yash Jankay <yashjankay@yashs-mbp.lan>
2024-09-08 20:24:30 -0700

    trying to fix beam

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <Script
           src="https://beamanalytics.b-cdn.net/beam.min.js"
           data-token="712ffbe5-3e6e-46bf-b828-a5c7f652dac4"
+          strategy="afterInteractive"
           async
         />
       </body>
