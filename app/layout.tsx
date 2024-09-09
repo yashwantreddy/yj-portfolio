e3654a3e406f69365f0070e39c27e99d902d8387
Yash Jankay <yashjankay@yashs-mbp.lan>
2024-09-08 20:39:21 -0700

    remove _document.js and fix head

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <Script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="712ffbe5-3e6e-46bf-b828-a5c7f652dac4"
+          strategy="afterInteractive"
+          async
+        />
+      </head>
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
